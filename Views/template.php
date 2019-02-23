d582aa97c7689516aa360de569503f7fc16c0f26
gilvan <g.r.almeida@live.com>
2019-02-23 15:26:16 -0300

    Adding Dashboard markup

--- Views/template.php
+++ Views/template.php
@@ -5,5 +5,47 @@
 <?php $this->loadView('menu', ['company_name' => $viewData['company_name']]); ?>
 <div class="container">
     <?php $this->loadView('top', ['user_email' => $viewData['user_email']]); ?>
+    <div class="dashboard">
+        <div class="grid_1">
+            <div class="dashboard_grid_area">
+                <div class="dashboard_grid_area_count">99</div>
+                <div class="dashboard_grid_area_legend">Legenda</div>
+            </div>
+        </div>
+        <div class="grid_1">
+            <div class="dashboard_grid_area">
+                <div class="dashboard_grid_area_count">99</div>
+                <div class="dashboard_grid_area_legend">Legenda</div>
+            </div>
+        </div>
+        <div class="grid_1">
+            <div class="dashboard_grid_area">
+                <div class="dashboard_grid_area_count">99</div>
+                <div class="dashboard_grid_area_legend">Legenda</div>
+            </div>
+        </div>
+    </div>
+    <div class="dashboard">
+        <div class="grid_2">
+            <div class="dashboard_info">
+                <div class="dashboard_info_title">
+                    Título
+                </div>
+                <div class="dashboard_info_body">
+                    Corpo
+                </div>
+            </div>
+        </div>
+        <div class="grid_1">
+            <div class="dashboard_info">
+                <div class="dashboard_info_title">
+                    Título
+                </div>
+                <div class="dashboard_info_body">
+                    Corpo
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php $this->loadView('footer'); ?>
